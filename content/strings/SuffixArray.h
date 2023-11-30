248c66e2f31122fb41a6597d970abd52d75030d0
Shi Zhengyu <shizhengyu93@hotmail.com>
2023-11-30 22:56:27 +0800

    remove extra folder

--- content/strings/SuffixArray.h
+++ content/strings/SuffixArray.h
@@ -1,122 +1,59 @@
 /**
- * Author: Lee Zongyu
+ * Author: pupu
  * Description: suffix array
  */
 
-typedef vector<ll> vl;
+struct SuffixArray {
+    static const int N = 1000005; // the length of the string
 
-class SuffixArray{
-    private:
-        ll max_range = 2e5+2; // TODO the maximum value (Ensure it won,t exceed the requirement)
-        ll n;
-        
-        void countingSort(int k){      
-            vl freq(max_range);   
-            for (int i = 0; i < n; ++i)                     // count the frequency
-                freq[ i + k < n ? rk[i+k] : 0]++;           // of each integer rank
-            for (int i = 0, sum = 0; i < max_range; ++i) {
-                int t = freq[i]; 
-                freq[i] = sum;
-                sum += t;
-            }
-            vl tempsa(n);
-            for (int i = 0; i < n; ++i)                      // sort SA
-            tempsa[ freq[ sa[i]+k < n ? rk[sa[i]+k] : 0] ++ ] = sa[i];
-            swap(sa, tempsa);                                   
+    int n, m, cnt[N], sa[N], rk[N], id[N];
+
+    void radixSort() {
+        for (int i = 0; i < m; ++i) {
+            cnt[i] = 0;
         }
+        for (int i = 0; i < n; ++i) {
+            ++cnt[rk[i]];
+        }
+        for (int i = 1; i < m; ++i) {
+            cnt[i] += cnt[i - 1];
+        }
+        for (int i = n - 1; ~i; --i) {
+            sa[--cnt[rk[id[i]]]] = id[i];
+        }
+    }
 
-        void constructLCP(){
-            vl phi(n);
-            vl plcp(n);
-            phi[sa[0]] = -1;
-            for(int i = 1; i < n; i++)
-                phi[sa[i]] = sa[i-1];
-            
-            for(int i = 0, l = 0; i < n; i ++ ){
-                if(phi[i] == -1){
-                    plcp[i] = 0;
-                    continue;
+    bool cmp(int x, int y, int l) {
+        return id[x] == id[y] && id[x + l] == id[y + l];
+    }
+
+    template<typename T>
+    void initSA(T first, T last) {
+        n = last - first, m = 0;
+        for (int i = 0; i < n; ++i) {
+            rk[i] = *(first + i);
+            m = std::max(m, rk[i] + 1);
+            id[i] = i;
+        }
+        radixSort();
+        for (int l = 1, p = 0; p < n && l < n; m = p, l <<= 1) {
+            p = 0;
+            for (int i = n - l; i < n; ++i) {
+                id[p++] = i;
+            }
+//            std::cout << "p = " << p << ' ' << l << std::endl;
+            for (int i = 0; i < n; ++i) {
+                if (sa[i] >= l && p < n) {
+                    id[p++] = sa[i] - l;
                 }
-                while((i+l < n ) && (phi[i] + l < n) && s[i+l] == s[phi[i] + l] ) l++;
-                plcp[i] = l;
-                l = max(l-1,0);
             }
-            lcp.resize(n);
-            for(int i = 0; i < n; i++)
-                lcp[i] = plcp[sa[i]];
-        }
-
-        void constructSA() {                           
-            // init
-            rk.assign(n,0);
-            sa.assign(n,0);
-            iota(sa.begin(), sa.end(), 0);               
-            for (int i = 0; i < n; ++i) rk[i] = s[i];    
-
-            // repeat log_2 n times
-            for (int k = 1; k < n; k <<= 1) {          
-                countingSort(k);                           // radix sort
-                countingSort(0);                           // stable-sort
-                vl temprk(n);
-                int r = 0;
-                temprk[sa[0]] = r;                         // re-ranking process
-                for (int i = 1; i < n; ++i)                // compare adj suffixes, same pair => same rank r; otherwise, increase r
-                    temprk[sa[i]] = 
-                    ((rk[sa[i]] == rk[sa[i-1]]) && (rk[sa[i]+k] == rk[sa[i-1]+k])) ?
-                        r : ++r;
-                swap(rk, temprk);                          // update RA
-
-                // constant optimization
-                max_range = r + 1;
-                if (rk[sa[n-1]] == n-1) break;             
+            radixSort();
+            for (int i = 0; i < n; ++i) id[i] = rk[i];
+            p = 1, rk[sa[0]] = 0;
+            for (int i = 1; i < n; ++i) {
+                if (!cmp(sa[i - 1], sa[i], l) && p < n) ++p;
+                rk[sa[i]] = p - 1;
             }
         }
-    // vl for s,a,q are changable to string
-    public:
-        vl rk, sa, lcp, owner; // 0 - index lcp in sa form between me and previous
-        vector<ll> s; 
-        void discrete(vector<ll> a){
-            set<ll> st;
-            for(int c:a) st.insert(c);
-            ll idx = 0;
-            unordered_map<ll,ll> mp;
-            for(int c:st) mp[c] = idx++;
-            s.assign(a.size(),0);
-            for(int i = 0; i < n; i++) s[i] = mp[a[i]];
-        }
-        SuffixArray(vector<ll> a){
-            n = a.size();
-            discrete(a); // Comment if discretization is unnecesary;
-            constructSA();
-            constructLCP();
-        }
-
-        int lower_bound(vl q){
-            int l = 1, r = n;
-            while(l<r){
-                int m = (l+r)/2;
-                if(compare(sa[m],q)>=0) r = m;
-                else l = m + 1;
-            }
-            return (l!=n)? sa[l]: -1;
-        }
-
-        int upper_bound(vl q){
-            int l = 1, r= n;
-            while(l < r){
-                int m = (l + r)/2;
-                if(compare(sa[m], q) > 0) r = m;
-                else l = m + 1;
-            }
-            return (l!=n)? sa[l] : -1;
-        }
-
-        int compare(int idx, vl q){
-            for(int i = 0; i < q.size(); i++){ 
-                if(s[idx+i] < q[i]) return -1;
-                else if(s[idx+i] > q[i]) return 1;
-            }
-            return 0;
-        }
-
-};
+    }
+} SA;
